57d7eb7b1143ab0d3c4cad33536e760dc8a11bfc
Sarita200 <saritasadgir1@gmail.com>
2024-07-21 13:51:26 +0530

    basic folser structure created in client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider , createBrowserRouter } from "react-router-dom"
+
+import Home from "./views/Home/Home"
+import SignUp from "./views/SignUp/SignUp"
+import Login from "./views/Login/Login"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   );
 
+  const router =  createBrowserRouter([
+  {
+    path :'/',
+    element : <Home/>
+  },
+  {
+    path :'/signup',
+    element : <SignUp/>
+  },
+  {
+    path :'/login',
+    element : <Login/>
+  },
+  {
+    path :'*',
+    element : <h1>Not Found</h1>
+  }
+])
 
+root.render(<RouterProvider router={router}/>)
